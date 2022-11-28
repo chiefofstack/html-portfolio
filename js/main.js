d92e8202ae9825a225e45e7660df2d792f89bc0d
chiefofstack <chiefofstack@gmail.com>
2022-11-28 17:19:16 +0000

    Complete the nav toggle button + fixed the conflict when user clicks outside the nav and the toggle button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Typing Effect
 const typing = document.getElementById('typing');
 var typewriter = new Typewriter(typing, {
     loop: true,
@@ -14,3 +15,48 @@ typewriter.typeString("Hey! My name is Mark Acab. ")
     .pauseFor(3000)
     .deleteAll()
     .start()
+
+
+
+
+    
+
+
+// Mobile Menu toggle
+const $nav = $('.sidebar > .nav');
+const $toggle = $('.btn-toggle');
+const $hamburger = $('.btn-toggle > .hamburger');
+
+// Toggle active state of button and nav 
+$('.btn-toggle').on('click', function(e){    
+
+    if($nav.hasClass('active')){
+        $nav.removeClass('active');
+        $hamburger.removeClass('active');       
+    }
+    else{
+        $nav.addClass('active');
+        $hamburger.addClass('active');       
+    }  
+    console.log($nav.hasClass('active'));
+});
+
+// Hide menu on window resize
+$(window).on("resize", function(event){
+    $nav.removeClass('active');
+    $hamburger.removeClass('active');
+});
+
+
+// Hide menu if clicked outside the toggler or navigation
+$(document).on('mouseup',function(e) 
+{   //do not process the mouse click if on toggle button
+    if(!($toggle.is(e.target) || $toggle.has(e.target).length)){ 
+        //remove active class if outside the navigation
+        if (!$nav.is(e.target) && $nav.has(e.target).length === 0) 
+        {
+            $nav.removeClass('active');        
+            $hamburger.removeClass('active');
+        } 
+    }   
+});
