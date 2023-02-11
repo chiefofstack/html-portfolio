1f2c7c37a2e91d2af4d351e3750f8111b568d407
chiefofstack <chiefofstack@gmail.com>
2023-02-11 15:49:21 +0000

    add new form field types to the validate method, add validation as you type on the input fields + refactor to improve the code readability

--- js/home.js
+++ js/home.js
@@ -21,99 +21,123 @@ typewriter.typeString("Hey! My name is Mark Acab. ")
 
 // Contact form validation
 // returns true if input is valid 
+
+
 function validate(field, label, required, type){
     const inputField = $(`#${field}`);    
-    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/; //email regex
-    const phonePattern = /^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/; //UK phone regex
-    let passed = true;
+    const namePattern = /^[a-zA-Z-\s']*$/; 
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/; 
+    const phonePattern = /^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/; //UK phone regex , 
     
-    // Validate required
-    if (required){        
-        if(inputField.val()==""){
-            inputField.addClass('input-field-error');
-            inputField.siblings('.input-error').text(`${label} is required`);
-            return false;
+    let error = false;
+    // If required and empty, show error and return false right away.
+    if (required && inputField.val() == ""){ 
+        inputField.addClass('input-field-error');
+        inputField.siblings('.input-error').text(`${label} is required`);
+        return false;       
+    }
+    
+    // Check lengths and patterns
+    // Name
+    if(type == 'name'){        
+        if (inputField.val().length > 35) {
+            error =  `${label} is too long`;
+        } 
+        else if (inputField.val().length  < 2) {
+            error =  `${label} is too short`;
         }
-        else{
-            // Validate patterns 
-            if(type == 'email'){
-                passed = emailPattern.test(inputField.val().trim());
-            }
-            else if(type == 'phone'){
-                passed = phonePattern.test(inputField.val().trim());
-            }
-            else{
-                passed = true;
-            }
-                        
-            //show errors
-            if(!passed ){
-                inputField.addClass('input-field-error');
-                inputField.siblings('.input-error').text(`Please enter a valid ${label}`);
-                return false;        
-            }
-            else{
-                inputField.removeClass('input-field-error');
-                inputField.siblings('.input-error').text('');
-                return true;
-            }        
+        else if(!namePattern.test(inputField.val().trim())){
+            error = `Please enter a valid ${label}`;
         }
+
+    }
+    // Email
+    else if(type == 'email'){
+        if (inputField.val().length > 254) {
+            error =  `${label} is too long`;
+        } 
+        else if(!emailPattern.test(inputField.val().trim())){
+            error = `Please enter a valid ${label}`;
+        } 
+    }
+    // Phone
+    else if(type == 'phone'){
+        if (inputField.val().length > 13) {
+            error =  `${label} is too long`;
+        } 
+        else if (inputField.val().length  < 11) {
+            error =  `${label} is too short`;
+        }
+        else if(!phonePattern.test(inputField.val().trim())){
+            error = `Please enter a valid ${label}`;
+        } 
+    }
+    // Subject
+    else if(type == 'subject'){
+        if (inputField.val().length > 254) {
+            error =  `${label} is too long`;
+        } 
+        else if (inputField.val().length  < 2) {
+            error =  `${label} is too short`;
+        }
+    }
+    // Message
+    else if(type == 'message'){
+        if (inputField.val().length > 2000) {
+            error =  `${label} is too long`;
+        } 
+        else if (inputField.val().length  < 2) {
+            error =  `${label} is too short`;
+        }
+    }
+
+    // Show and return result
+    if(error){
+        inputField.addClass('input-field-error');
+        inputField.siblings('.input-error').text(error);
+        return false;        
     }
     else{
-        if(inputField.val()!=""){
-            // Validate patterns 
-            if(type == 'email'){
-                passed = emailPattern.test(inputField.val().trim());
-            }
-            else if(type == 'phone'){
-                passed = phonePattern.test(inputField.val().trim());
-            }
-            else{
-                passed = true;
-            }
-                        
-            //show errors
-            if(!passed ){
-                inputField.addClass('input-field-error');
-                inputField.siblings('.input-error').text(`Please enter a valid ${label}`);
-                return false;        
-            }
-            else{
-                inputField.removeClass('input-field-error');
-                inputField.siblings('.input-error').text('');
-                return true;
-            }       
-        }
-        else{
-            return true;            
-        }
+        inputField.removeClass('input-field-error');
+        inputField.siblings('.input-error').text('');
+        return true;
     }
+    
 }
 
-
-$('#sendMessage').on('click', ()=>{    
-    const firstName = $('#firstName');
-    let errorCount = 0;
-
-    if(!validate('firstName','First name', true, "text")) {errorCount++;}
-    if(!validate('lastName','Last name', true,"text")) {errorCount++;}
-    if(!validate('email','Email', true, "email")) {errorCount++;}
-    if(!validate('telephone','Telephone', false, 'phone')) {errorCount++;}
-    if(!validate('subject','Subject', true, "text")) {errorCount++;}
-    if(!validate('message-field','Message', true, "text")) {errorCount++;}
-
-
+function displayFormAlert(errorCount, displayName = null){
     if(errorCount > 0){
         $('.form-message').removeClass('success');
         $('.form-message').addClass('not-success');
         $('.form-message').text(`Sorry there were ${errorCount} errors in the form.`);
     }
     else{
-        let displayName = firstName.val();
         $('form :input').val('');
         $('.form-message').removeClass('not-success');
         $('.form-message').addClass('success');
         $('.form-message').text(`Thanks ${displayName}! your message has been sent.`);
     }
+}
 
+// Check each input field
+$('#firstName').on('input', ()=>{ validate('firstName','First name', true, "name"); });
+$('#lastName').on('input', ()=>{ validate('lastName','Last name', true, "name"); });
+$('#email').on('input', ()=>{ validate('email','Email', true, "email"); });
+$('#telephone').on('input', ()=>{ validate('telephone','Telephone', true, "phone"); });
+$('#subject').on('input', ()=>{ validate('subject','Subject', true, "subject"); });
+$('#message-field').on('input', ()=>{ validate('message-field','Message', true, "message"); });
+
+// Check all fields
+$('#sendMessage').on('click', ()=>{    
+    const displayName = $('#firstName').val();
+    let errorCount = 0;
+
+    !validate('firstName','First name', true, "name") && errorCount++
+    !validate('lastName','Last name', true,"name") && errorCount++
+    !validate('email','Email', true, "email") && errorCount++
+    !validate('telephone','Telephone', false, 'phone') && errorCount++
+    !validate('subject','Subject', true, "subject") && errorCount++
+    !validate('message-field','Message', true, "message") && errorCount++
+
+    displayFormAlert(errorCount, displayName);
 });
