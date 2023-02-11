c32a9e26e2616b3f6708ebbf10ef64453333c085
chiefofstack <chiefofstack@gmail.com>
2023-02-11 15:45:53 +0000

    replace .html with .php + use the new sidebar.php

--- scs-scheme.php
+++ scs-scheme.php
@@ -23,49 +23,7 @@
         <link rel="stylesheet" href="css/application.css">
     </head>
     <body class="grid">
-        <!-- Sidebar Nav-->
-        <header class="sidebar">
-            <a class="site-logo" href="index.html#home">
-                <b class="srt">Mark Jason Acab</b>
-            </a>
-            <a href="#" class="btn btn-toggle">
-                <span class="hamburger">
-                    <b class="srt">Toggle</b>
-                </span>
-            </a>   
-            <ul class="nav">
-                <li class="nav__item">
-                    <a href="about.html" class="">                            
-                        About Me
-                    </a>                    
-                </li>
-                <li class="nav__item">
-                    <a href="index.html#portfolio" class="">                            
-                        My Portfolio
-                    </a>                    
-                </li>
-                <li class="nav__item">
-                    <a href="coding-examples.html" class="">                            
-                        Coding Examples
-                    </a>                    
-                </li>
-                <li class="nav__item">
-                    <a href="scs-scheme.html" class="">                            
-                        SCS Scheme
-                    </a>                    
-                </li>
-                <li class="nav__item">
-                    <a href="index.html#contact" class="">                            
-                        Contact Me
-                    </a>                    
-                </li>
-            </ul>           
-            <div class="social">
-                <a href="https://github.com/chiefofstack" target="_blank" class="btn"><i class="fa fa-github"></i></a>
-                <a href="https://www.linkedin.com/in/jayacab/" target="_blank" class="btn"><i class="fa fa-linkedin"></i></a>
-            </div>
-
-        </header>
+        <?php require 'includes/sidebar.php'; ?>
 
         <!-- Main Content -->
         <main class="content">
