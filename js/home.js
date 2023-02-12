750da96db0bbe75dab0d3a9912572315e642c645
chiefofstack <chiefofstack@gmail.com>
2023-02-12 01:19:13 +0000

    add textPattern for subject and message

--- js/home.js
+++ js/home.js
@@ -26,6 +26,7 @@ function validate(field, label, required, type){
     const namePattern = /^[a-zA-Z-\s']*$/; 
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/; 
     const phonePattern = /^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/; //UK phone regex , 
+    const textPattern = /^[\w\-\s\.\,]+$/; 
     
     let error = false;
     // If required and empty, show error and return false right away.
@@ -78,6 +79,9 @@ function validate(field, label, required, type){
         else if (inputField.val().length  < 2) {
             error =  `${label} is too short`;
         }
+        else if(!textPattern.test(inputField.val().trim())){
+            error = `${label} not alphanumeric`;
+        } 
     }
     // Message
     else if(type == 'message'){
@@ -87,6 +91,9 @@ function validate(field, label, required, type){
         else if (inputField.val().length  < 2) {
             error =  `${label} is too short`;
         }
+        else if(!textPattern.test(inputField.val().trim())){
+            error = `${label}  not alphanumeric`;
+        } 
     }
 
     // Show and return result
