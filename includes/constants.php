3e7f1de30e6f82e8085afec68ec911900c94a2be
chiefofstack <chiefofstack@gmail.com>
2023-02-12 01:18:46 +0000

    update database name

--- includes/constants.php
+++ includes/constants.php
@@ -4,6 +4,6 @@
 define("DB_SERVER", "localhost");
 define("DB_USER", "developer");
 define("DB_PASS", "developer");
-define("DB_NAME", "netmatters");
+define("DB_NAME", "netmatters_portfolio");
 
 ?>
